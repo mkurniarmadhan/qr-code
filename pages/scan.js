7f08aedf57f0785b51d3d194619a3d06a14fdf43
Amar <mkurniarmadhan@gmail.com>
2022-12-20 18:41:34 +0700

    first commit

--- pages/scan.js
+++ pages/scan.js
@@ -1,6 +1,6 @@
 import React, { useState, useRef } from "react";
-import QrReader from "react-qr-scanner";
-import styles from "../../styles/Home.module.css";
+import { QrReader } from "react-qr-reader";
+import styles from "../styles/Home.module.css";
 
 function Scan() {
   const [data, setData] = useState("No result");
@@ -9,15 +9,16 @@ function Scan() {
     <div className={styles.container}>
       <div className={styles.container}>
         <QrReader
-          onScan={(result, error) => {
+          onResult={(result, error) => {
             if (!!result) {
               setData(result?.text);
             }
+
             if (!!error) {
               console.info(error);
             }
           }}
-          // constraints={{ facingMode: "environment" }}
+          constraints={{ facingMode: "environment" }}
           style={{ width: "40%", height: "40%" }}
         />
         <p>{data}</p>
